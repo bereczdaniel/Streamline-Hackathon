9a1a622934b329c5be3cc03b3971b873c6f3a1cd
bdaniel <berecz.17@gmail.com>
2018-06-30 18:12:36 +0200

    exceptionhangdling

--- hackathon-flink-scala/src/main/scala/eu/streamline/hackathon/flink/scala/job/IO/HttpSink.scala
+++ hackathon-flink-scala/src/main/scala/eu/streamline/hackathon/flink/scala/job/IO/HttpSink.scala
@@ -4,12 +4,15 @@ import com.google.gson.Gson
 import eu.streamline.hackathon.flink.scala.job.utils.Types.BasicPostLoad
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction
+import org.apache.http.NoHttpResponseException
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.DefaultHttpClient
+import org.slf4j.LoggerFactory
 
 class HttpSink[T <: BasicPostLoad] extends RichSinkFunction[T]{
 
+  private val log = LoggerFactory.getLogger(classOf[HttpSink[T]])
   lazy val url = "http://localhost:8081/"
   lazy val post = new HttpPost(url)
   def client = new DefaultHttpClient
@@ -26,8 +29,13 @@ class HttpSink[T <: BasicPostLoad] extends RichSinkFunction[T]{
     post.setEntity(new StringEntity(new Gson().toJson(in)))
 
 
-    // send the post request
-    client.execute(post)
+    try {
+      // send the post request
+      client.execute(post)
+    }
+    catch {
+      case NoHttpResponseException => log.error("NoHttpResponseException: lost info of" + in)
+    }
 
     client.close()
   }
