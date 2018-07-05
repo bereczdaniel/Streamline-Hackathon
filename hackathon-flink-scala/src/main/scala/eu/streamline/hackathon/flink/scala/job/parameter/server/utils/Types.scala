e52a007fec2084193dcb78fccca9bf73cfb198d5
bdaniel <berecz.17@gmail.com>
2018-07-05 10:27:23 +0200

    refactor Types

--- hackathon-flink-scala/src/main/scala/eu/streamline/hackathon/flink/scala/job/parameter/server/utils/Types.scala
+++ hackathon-flink-scala/src/main/scala/eu/streamline/hackathon/flink/scala/job/parameter/server/utils/Types.scala
@@ -2,31 +2,40 @@ package eu.streamline.hackathon.flink.scala.job.parameter.server.utils
 
 object Types {
 
-  case class Rating(userId: UserId, itemId: ItemId, rating: Double)
+  sealed abstract class WorkerInput(val id: Int)
+  case class Rating(userId: UserId, itemId: ItemId, rating: Double) extends WorkerInput(itemId)
+  case class EvaluationRequest(userId: UserId, itemId: ItemId, recommendationId: Int, rating: Rating) extends WorkerInput(itemId)
+  case class RecommendationRequest(userId: UserId, recommendationId: Int) extends WorkerInput(userId)
+  case class NegativeExample(userId: UserId, itemId: ItemId, rating: Rating) extends WorkerInput(itemId)
 
-  sealed abstract class ParameterOutput(val id: Int, val parameter: Parameter){
+  sealed trait ParameterServerOutput
+  case class VectorModelOutput(id: Int, parameter: Parameter) extends ParameterServerOutput {
     override def toString: String =
       id.toString + ":" + parameter.tail.foldLeft(parameter.head.toString)((acc, c) => acc + "," + c.toString)
   }
-  case class WorkerOut(itemId: Int, params: Parameter) extends ParameterOutput(itemId, params)
-  case class ServerOut(userId: Int, params: Parameter) extends ParameterOutput(userId, params)
-  case class ServerToWorker(id: Int, source: Int, parameter: Parameter) {
+  case class RecommendationOutput(userId: UserId, topK: TopK) extends ParameterServerOutput {
     override def toString: String =
-      id.toString + ":" + source.toString + ":" + parameter.tail.foldLeft(parameter.head.toString)((acc, c) => acc + "," + c.toString)
+      userId.toString + ":" + topK.tail.foldLeft(topK.head.toString)((acc, c) => acc + "," + c.toString)
   }
 
-  def serverToWorkerFromString(line: String): ServerToWorker = {
+
+  sealed abstract class Message(val destination: Int, val source: Int)
+  case class PullAnswer(targetId: Int, override val source: Int, parameter: Parameter) extends Message(targetId, source) {
+    override def toString: String =
+      targetId.toString + ":" + source.toString + ":" + parameter.tail.foldLeft(parameter.head.toString)((acc, c) => acc + "," + c.toString)
+  }
+
+  def pullAnswerFromString(line: String): PullAnswer = {
     val fields = line.split(":")
-    ServerToWorker(fields(0).toInt, fields(1).toInt, fields(2).split(",").map(_.toDouble))
+    PullAnswer(fields(0).toInt, fields(1).toInt, fields(2).split(",").map(_.toDouble))
   }
 
-  sealed abstract class WorkerToServer(val id: Int)
-  case class Push(targetId: Int, parameter: Parameter) extends WorkerToServer(targetId){
+  case class Push(targetId: Int, parameter: Parameter) extends Message(targetId, -1){
     override def toString: String = {
       "Push:" + targetId.toString + ":" + parameter.tail.foldLeft(parameter.head.toString)((acc, c) => acc + "," + c.toString)
     }
   }
-  case class Pull(targetId: Int, source: Int) extends WorkerToServer(targetId) {
+  case class Pull(targetId: Int, override val source: Int) extends Message(targetId, source) {
     override def toString: String =
       "Pull:" + targetId.toString + ":" + source.toString
   }
@@ -34,12 +43,16 @@ object Types {
   type Parameter = Array[Double]
   type UserId = Int
   type ItemId = Int
+  type TopK = Array[ItemId]
 
 
   /**
     * Exception to be thrown when a vector addition results in a NaN
     */
   class FactorIsNotANumberException extends Exception
+  class NotSupportedWorkerInput extends Exception
+  class NotSupportedMessage extends Exception
+  class NotSupportedOutput extends Exception
 
   def vectorSum(u: Parameter, v: Parameter ): Array[Double] = {
     val n = u.length
